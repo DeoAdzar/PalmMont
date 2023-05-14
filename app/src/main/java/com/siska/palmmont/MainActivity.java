59793fb66acd54a6d799a36afdffb3c8cca12cd1
DeoAdzarN <deo.nugraha@woilo.com>
2023-05-15 04:18:26 +0700

    tambahan

--- app/src/main/java/com/siska/palmmont/MainActivity.java
+++ app/src/main/java/com/siska/palmmont/MainActivity.java
@@ -1,10 +1,14 @@
 package com.siska.palmmont;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -14,6 +18,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -25,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        Window window = this.getWindow();
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.white));
         setContentView(R.layout.activity_main);
         displayMoisture = findViewById(R.id.displayMoisture);
         displayPh = findViewById(R.id.displayPh);
@@ -46,11 +60,13 @@ public class MainActivity extends AppCompatActivity {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 ObjectData data = dataSnapshot.getValue(ObjectData.class);
-                assert data != null;
-                displayMoisture.setText(data.getMoisture());
-                displayTemp.setText(data.getTemp());
-                displayPh.setText(data.getPh());
-                pump.setChecked(data.isPump());
+                if (data!=null){
+                    displayMoisture.setText(data.getMoisture());
+                    displayTemp.setText(data.getTemp());
+                    displayPh.setText(data.getPh());
+                    pump.setChecked(data.isPump());
+                }
+
             }
 
             @Override
